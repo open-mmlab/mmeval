49dca40db63481332dd0c28b313e51d06c7edf2c
yancong <xych6@outlook.com>
2022-08-16 19:05:48 +0800

    dev(mmeval/core/dispatch): add multiple dispatch

--- mmeval/core/__init__.py
+++ mmeval/core/__init__.py
@@ -1,10 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
 from mmeval.core import dist_backends
+from mmeval.core.dispatcher import dispatch
 from mmeval.core.dist import (get_dist_backend, list_all_backends,
                               set_default_dist_backend)
 
 __all__ = [
     'dist_backends', 'get_dist_backend', 'set_default_dist_backend',
-    'list_all_backends'
+    'list_all_backends', 'dispatch'
 ]
