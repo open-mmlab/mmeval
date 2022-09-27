10810a20986b963551f7d6563acf66510ba42474
yancong <xych6@outlook.com>
2022-09-27 22:29:02 +0800

    detection): import detection

--- mmeval/__init__.py
+++ mmeval/__init__.py
@@ -1,6 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
-from mmeval import core
-from .version import __version__  # noqa: F401
+# flake8: noqa
+
+from .core import *
+from .detection import *
+from .version import __version__
 
 __all__ = ['core', '__version__']
