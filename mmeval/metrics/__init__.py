8b30716df8d3953115296a15263ea4a207bb6178
daiwenxun.vendor <daiwenxun@pjlab.org.cn>
2022-10-20 14:47:05 +0800

    first commit

--- mmeval/metrics/__init__.py
+++ mmeval/metrics/__init__.py
@@ -5,5 +5,6 @@ from .f_metric import F1Metric
 from .mean_iou import MeanIoU
 from .oid_map import OIDMeanAP
 from .voc_map import VOCMeanAP
+from .ava_map import AVAMeanAP
 
-__all__ = ['Accuracy', 'MeanIoU', 'VOCMeanAP', 'OIDMeanAP', 'F1Metric']
+__all__ = ['Accuracy', 'MeanIoU', 'VOCMeanAP', 'OIDMeanAP', 'F1Metric', 'AVAMeanAP']
