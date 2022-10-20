2744914ea38e8d3e51ee85f95e2efa3cbdfaebfc
Zachary-66 <52512976+Zachary-66@users.noreply.github.com>
2022-10-20 21:57:37 +0800

    fix the format

--- mmeval/metrics/end_point_error.py
+++ mmeval/metrics/end_point_error.py
@@ -94,7 +94,8 @@ class EndPointError(BaseMetric):
                 valid_mask = valid_masks[idx]
             else:
                 valid_mask = None
-            epe, num_valid_pixel = self.end_point_error_map(prediction, label, valid_mask)
+            epe, num_valid_pixel = self.end_point_error_map(
+                prediction, label, valid_mask)
             self._results.append((epe, num_valid_pixel))
 
     @overload  # type: ignore
