8d1480c31c7a11db0139f3924d27e8b306f17ca8
Dai-Wenxun <wxdai2001@gmail.com>
2022-10-24 21:38:06 +0800

    fix init

--- mmeval/metrics/__init__.py
+++ mmeval/metrics/__init__.py
@@ -6,6 +6,7 @@ from .f_metric import F1Metric
 from .hmean_iou import HmeanIoU
 from .mean_iou import MeanIoU
 from .oid_map import OIDMeanAP
+from .voc_map import VOCMeanAP
 
 __all__ = [
     'Accuracy', 'MeanIoU', 'VOCMeanAP', 'OIDMeanAP', 'HmeanIoU', 'F1Metric',
