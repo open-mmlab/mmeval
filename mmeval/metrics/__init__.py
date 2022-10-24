4cc44198eada5831098c42ffe146dfead63a2b05
Dai-Wenxun <wxdai2001@gmail.com>
2022-10-24 21:31:43 +0800

    add init

--- mmeval/metrics/__init__.py
+++ mmeval/metrics/__init__.py
@@ -1,12 +1,13 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
 from .accuracy import Accuracy
+from .ava_map import AVAMeanAP
 from .f_metric import F1Metric
 from .hmean_iou import HmeanIoU
 from .mean_iou import MeanIoU
 from .oid_map import OIDMeanAP
-from .voc_map import VOCMeanAP
 
 __all__ = [
-    'Accuracy', 'MeanIoU', 'VOCMeanAP', 'OIDMeanAP', 'HmeanIoU', 'F1Metric'
+    'Accuracy', 'MeanIoU', 'VOCMeanAP', 'OIDMeanAP', 'HmeanIoU', 'F1Metric',
+    'AVAMeanAP'
 ]
