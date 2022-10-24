c9d0441de564c4d9025b046cf48f0fa5c9dabdcc
Zachary-66 <1032903371@qq.com>
2022-10-24 11:26:20 +0800

    fix conflict

--- mmeval/metrics/__init__.py
+++ mmeval/metrics/__init__.py
@@ -2,12 +2,8 @@
 
 from .accuracy import Accuracy
 from .end_point_error import EndPointError
-from .f_metric import F1Metric
 from .mean_iou import MeanIoU
 from .oid_map import OIDMeanAP
 from .voc_map import VOCMeanAP
 
-__all__ = [
-    'Accuracy', 'MeanIoU', 'VOCMeanAP', 'OIDMeanAP', 'EndPointError',
-    'F1Metric'
-]
+__all__ = ['Accuracy', 'MeanIoU', 'VOCMeanAP', 'OIDMeanAP', 'EndPointError']
