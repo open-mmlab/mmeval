8b43ac1a747dc36addd4e49171a5bd858e822c4f
Z-Fran <1396925302@qq.com>
2022-10-26 15:31:37 +0800

    fix conflicts

--- mmeval/metrics/__init__.py
+++ mmeval/metrics/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
 from .accuracy import Accuracy
+from .coco_detection import COCODetectionMetric
 from .end_point_error import EndPointError
 from .f_metric import F1Metric
 from .hmean_iou import HmeanIoU
@@ -8,13 +9,13 @@ from .mae import MAE
 from .mean_iou import MeanIoU
 from .mse import MSE
 from .oid_map import OIDMeanAP
-from .pck_accuracy import JhmdbPCKAccuracy, MpiiPCKAccuracy
+from .pck_accuracy import JhmdbPCKAccuracy, MpiiPCKAccuracy, PCKAccuracy
 from .psnr import PSNR
 from .single_label import SingleLabelMetric
 from .voc_map import VOCMeanAP
 
 __all__ = [
     'Accuracy', 'MeanIoU', 'VOCMeanAP', 'OIDMeanAP', 'EndPointError',
-    'F1Metric', 'HmeanIoU', 'SingleLabelMetric', 'COCODetectionMetric'
+    'F1Metric', 'HmeanIoU', 'SingleLabelMetric', 'COCODetectionMetric',
     'PCKAccuracy', 'MpiiPCKAccuracy', 'JhmdbPCKAccuracy', 'PSNR', 'MAE', 'MSE'
 ]
