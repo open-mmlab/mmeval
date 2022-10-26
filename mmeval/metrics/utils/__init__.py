b344fde39b265e454c11ff3d63573a57989bb7f5
Dai-Wenxun <wxdai2001@gmail.com>
2022-10-26 13:54:28 +0800

    fix lint

--- mmeval/metrics/utils/__init__.py
+++ mmeval/metrics/utils/__init__.py
@@ -1,11 +1,18 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
+from .ava_evaluation import object_detection_evaluation, standard_fields
 from .hmean import compute_hmean
 from .polygon import (poly2shapely, poly_intersection, poly_iou,
                       poly_make_valid, poly_union, polys2shapely)
-from .ava_evaluation import *
 
 __all__ = [
-    'poly2shapely', 'polys2shapely', 'poly_union', 'poly_intersection',
-    'poly_make_valid', 'poly_iou', 'compute_hmean'
+    'poly2shapely',
+    'polys2shapely',
+    'poly_union',
+    'poly_intersection',
+    'poly_make_valid',
+    'poly_iou',
+    'compute_hmean',
+    'standard_fields',
+    'object_detection_evaluation',
 ]
