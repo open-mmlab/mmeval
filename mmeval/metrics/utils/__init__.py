dbd7ee508b9f538191b20d6bd75d1ba626a29225
Dai-Wenxun <wxdai2001@gmail.com>
2022-10-26 22:16:52 +0800

    fix lint

--- mmeval/metrics/utils/__init__.py
+++ mmeval/metrics/utils/__init__.py
@@ -9,8 +9,17 @@ from .polygon import (poly2shapely, poly_intersection, poly_iou,
                       poly_make_valid, poly_union, polys2shapely)
 
 __all__ = [
-    'poly2shapely', 'polys2shapely', 'poly_union', 'poly_intersection',
-    'poly_make_valid', 'poly_iou', 'compute_hmean', 'keypoint_pck_accuracy',
-    'calculate_overlaps', 'calculate_bboxes_area', 'reorder_and_crop',
-    'standard_fields', 'object_detection_evaluation',
+    'poly2shapely',
+    'polys2shapely',
+    'poly_union',
+    'poly_intersection',
+    'poly_make_valid',
+    'poly_iou',
+    'compute_hmean',
+    'keypoint_pck_accuracy',
+    'calculate_overlaps',
+    'calculate_bboxes_area',
+    'reorder_and_crop',
+    'standard_fields',
+    'object_detection_evaluation',
 ]
