17731b8ff1fe864e45c4375cd57c6d3cb6c3f516
Dai-Wenxun <wxdai2001@gmail.com>
2022-10-27 16:32:16 +0800

    fix

--- mmeval/metrics/_vendor/__init__.py
+++ mmeval/metrics/_vendor/__init__.py
@@ -2,7 +2,4 @@
 
 from .ava_evaluation import object_detection_evaluation, standard_fields
 
-__all__ = [
-    'standard_fields',
-    'object_detection_evaluation',
-]
+__all__ = ['standard_fields', 'object_detection_evaluation']
