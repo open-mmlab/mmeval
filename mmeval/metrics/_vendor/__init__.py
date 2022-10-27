138885944d90dc87feca46586ae98be505b84388
Dai-Wenxun <wxdai2001@gmail.com>
2022-10-27 16:35:28 +0800

    fix

--- mmeval/metrics/_vendor/__init__.py
+++ mmeval/metrics/_vendor/__init__.py
@@ -1,5 +1,1 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-
-from .ava_evaluation import object_detection_evaluation, standard_fields
-
-__all__ = ['standard_fields', 'object_detection_evaluation']
