6b5a5328ffa6927569874b5c4c7eb878ee68509c
ofhwei <hwei1023@gmail.com>
2022-12-05 12:47:57 +0000

    refine

--- tests/test_core/test_dist_backends/test_oneflow_dist_single_node_multi_processes.py
+++ tests/test_core/test_dist_backends/test_oneflow_dist_single_node_multi_processes.py
@@ -1,8 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
-import os, sys
-import pytest
 import numpy as np
+import os
+import pytest
+import sys
 
 # check if current process is launch via mpirun
 if os.environ.get('OMPI_COMM_WORLD_SIZE', '0') != '0':
@@ -12,17 +13,19 @@ from mmeval.core.dist_backends.oneflow_dist import OneFlowDist
 
 flow = pytest.importorskip('oneflow')
 
+
 def equal(a, b):
     if isinstance(a, dict):
         return all(equal(a[k], b[k]) for k in a.keys())
     elif isinstance(a, (list, tuple)):
-        return all(equal(ai,bi) for ai,bi in zip(a,b))
+        return all(equal(ai, bi) for ai, bi in zip(a, b))
     elif isinstance(a, (int, float, bool, str)):
-        return a==b
+        return a == b
     elif isinstance(a, flow.Tensor):
-        return np.all(a.numpy()==b.numpy())
+        return np.all(a.numpy() == b.numpy())
     else:
         return False
+
 
 def _create_obj_list(world_size):
     obj_list = []
@@ -34,16 +37,20 @@ def _create_obj_list(world_size):
         obj_list.append(obj)
     return obj_list
 
-def _create_tensor_list(rank, world_size, device="cpu"):
+
+def _create_tensor_list(rank, world_size, device='cpu'):
     obj_list = []
     for idx in range(world_size):
         obj = dict()
         obj['rank'] = idx
         obj['world_size'] = world_size
         idx += 1
-        obj['data'] = [flow.tensor([idx * 1.0, idx * 2.0, idx * 3.0], device=device)]
+        obj['data'] = [
+            flow.tensor([idx * 1.0, idx * 2.0, idx * 3.0], device=device)
+        ]
         obj_list.append(obj)
     return obj_list
+
 
 def _oneflow_dist_all_gather_fn(rank, world_size, device):
     dist_comm = OneFlowDist()
@@ -70,8 +77,8 @@ def _oneflow_dist_all_gather_fn(rank, world_size, device):
 
     gather_obj_list = dist_comm.all_gather_object(local_obj)
     print(f'rank {rank}, gather_obj_list {gather_obj_list}')
-    
-    assert equal(gather_obj_list, obj_list), f"{gather_obj_list}, {obj_list}"
+
+    assert equal(gather_obj_list, obj_list), f'{gather_obj_list}, {obj_list}'
 
 
 def _oneflow_dist_broadcast_fn(rank, world_size, device):
@@ -97,7 +104,7 @@ def _oneflow_dist_broadcast_fn(rank, world_size, device):
     assert equal(broadcast_obj, rank_0_obj)
 
     # cuda
-    rank_0_obj = flow.randn(3,4).to(device)
+    rank_0_obj = flow.randn(3, 4).to(device)
 
     print(f'rank {rank}, obj {rank_0_obj}')
     broadcast_obj = dist_comm.broadcast_object(rank_0_obj, src=0)
@@ -106,17 +113,16 @@ def _oneflow_dist_broadcast_fn(rank, world_size, device):
     assert equal(broadcast_obj, rank_0_obj)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     fn = sys.argv[2]
     device = sys.argv[4]
-    assert fn in ["all_gather", "broadcast"]
-    assert device in ["cpu", "cuda"]
+    assert fn in ['all_gather', 'broadcast']
+    assert device in ['cpu', 'cuda']
     rank = flow.env.get_local_rank()
     world_size = flow.env.get_world_size()
 
-    device = f"{device}:{rank}"
-    if fn=="all_gather":
+    device = f'{device}:{rank}'
+    if fn == 'all_gather':
         _oneflow_dist_all_gather_fn(rank, world_size, device)
     else:
         _oneflow_dist_broadcast_fn(rank, world_size, device)
-
