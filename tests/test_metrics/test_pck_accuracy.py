bbffc9c312b264c93f8aebe8b0e71ee266f8ab44
yancong <xych6@outlook.com>
2022-12-21 14:45:35 +0800

    update test_pck_accuracy.py

--- tests/test_metrics/test_pck_accuracy.py
+++ tests/test_metrics/test_pck_accuracy.py
@@ -1,206 +1,215 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import numpy as np
-from unittest import TestCase
+import pytest
 
 from mmeval.metrics import JhmdbPCKAccuracy, MpiiPCKAccuracy, PCKAccuracy
 
 
-class TestPCKAccuracy(TestCase):
+# yapf: disable
+@pytest.mark.parametrize(
+    argnames='metric_kwargs',
+    argvalues=[
+        {},
+        {'thr': 1.0, 'norm_item': 'bbox'},
+        {'thr': 0.2, 'norm_item': 'head'},
+        {'thr': 0.2, 'norm_item': 'torso'},
+        {'thr': 0.2, 'norm_item': ['bbox', 'torso']},
+    ]
+)
+# yapf: enable
+def test_pck_accuracy_interface(metric_kwargs):
+    num_keypoints = 4
+    keypoints = np.random.random((1, num_keypoints, 2)) * 10
+    prediction = {'coords': keypoints}
+    keypoints_visible = np.ones((1, num_keypoints)).astype(bool)
+    groundtruth = {
+        'coords': keypoints,
+        'mask': keypoints_visible,
+        'bbox_size': np.random.random((1, 2)) * 10,  # for `bbox` norm_item
+        'head_size': np.random.random((1, 2)) * 10,  # for `head` norm_item
+        'torso_size': np.random.random((1, 2)) * 10,  # for `torso` norm_item
+    }
+    pck_accuracy = PCKAccuracy(**metric_kwargs)
+    results = pck_accuracy([prediction], [groundtruth])
+    assert isinstance(results, dict)
 
-    def setUp(self):
-        """Setup some variables which are used in every test method.
 
-        TestCase calls functions in this order: setUp() -> testMethod() ->
-        tearDown() -> cleanUp()
-        """
-        self.batch_size = 8
-        num_keypoints = 15
-        self.predictions = []
-        self.groundtruths = []
+# yapf: disable
+@pytest.mark.parametrize(
+    argnames=['metric_kwargs', 'prediction', 'groundtruth', 'results'],
+    argvalues=[
+        (
+            {'thr': 0.2, 'norm_item': 'bbox'},
+            {
+                'coords': np.array([[
+                    [2, 4], [6, 8], [6, 9], [7, 8]]])
+            },
+            {
+                'coords': np.array([[
+                    [2, 4], [6, 8], [6, 7], [7, 8]]]),
+                'mask': np.array([[True, True, True, True]]),
+                'bbox_size': np.array([[10, 10]])
+            },
+            {'PCK@0.2': 0.75}
+        )
+    ]
+)
+# yapf: enable
+def test_pck_accuracy_accurate(metric_kwargs, prediction, groundtruth,
+                               results):
+    pck_accuracy = PCKAccuracy(**metric_kwargs)
+    assert pck_accuracy([prediction], [groundtruth]) == results
 
-        for i in range(self.batch_size):
-            keypoints = np.zeros((1, num_keypoints, 2))
-            keypoints[0, i] = [0.5 * i, 0.5 * i]
-            keypoints_visible = np.ones((1, num_keypoints)).astype(bool)
-            keypoints_visible[0, (2 * i) % 8] = False
-            bbox_size = np.random.random((1, 2)) * 20 * i
-            head_size = np.random.random((1, 2)) * 10 * i
-            torso_size = np.random.random((1, 2)) * 15 * i
 
-            prediction = {'coords': keypoints}
-            groundtruth = {
-                'coords': keypoints,
-                'mask': keypoints_visible,
-                'bbox_size': bbox_size,
-                'head_size': head_size,
-                'torso_size': torso_size
+# yapf: disable
+@pytest.mark.parametrize(
+    argnames='metric_kwargs',
+    argvalues=[
+        {},
+        {'thr': 1.0, 'norm_item': 'bbox'},
+        {'thr': 0.2, 'norm_item': 'head'},
+        {'thr': 0.2, 'norm_item': 'torso'},
+        {'thr': 0.2, 'norm_item': ['bbox', 'torso']},
+    ]
+)
+# yapf: enable
+def test_jhmdb_pck_accuracy_interface(metric_kwargs):
+    num_keypoints = 15
+    keypoints = np.random.random((1, num_keypoints, 2)) * 10
+    prediction = {'coords': keypoints}
+    keypoints_visible = np.ones((1, num_keypoints)).astype(bool)
+    groundtruth = {
+        'coords': keypoints,
+        'mask': keypoints_visible,
+        'bbox_size': np.random.random((1, 2)) * 10,  # for `bbox` norm_item
+        'head_size': np.random.random((1, 2)) * 10,  # for `head` norm_item
+        'torso_size': np.random.random((1, 2)) * 10,  # for `torso` norm_item
+    }
+    jhmdb_pck_accuracy = JhmdbPCKAccuracy(**metric_kwargs)
+    results = jhmdb_pck_accuracy([prediction], [groundtruth])
+    assert isinstance(results, dict)
+
+
+# yapf: disable
+@pytest.mark.parametrize(
+    argnames=['metric_kwargs', 'prediction', 'groundtruth', 'results'],
+    argvalues=[
+        (
+            {'thr': 0.2, 'norm_item': 'bbox'},
+            {
+                'coords': np.array([[
+                    [2, 4], [6, 8], [6, 9], [7, 8], [3, 4],
+                    [7, 8], [8, 9], [8, 8], [1, 9], [2, 8],
+                    [4, 8], [6, 9], [6, 2], [1, 2], [2, 3]]])
+            },
+            {
+                'coords': np.array([[
+                    [1, 4], [6, 8], [6, 9], [7, 8], [3, 4],
+                    [1, 8], [8, 9], [8, 8], [1, 9], [2, 8],
+                    [1, 7], [6, 9], [6, 2], [1, 2], [2, 3]]]),
+                'mask': np.array([[
+                    True, True, True, True, True, True, True, True, True, True,
+                    True, True, True, True, True]]),
+                'bbox_size': np.array([[10, 10]])
+            },
+            {
+                'PCK@0.2': 0.866666666,
+                'Head PCK': 1.0,
+                'Sho PCK': 1.0,
+                'Elb PCK': 1.0,
+                'Wri PCK': 1.0,
+                'Hip PCK': 0.5,
+                'Knee PCK': 0.5,
+                'Ank PCK': 1.0,
+                'Mean PCK': 0.86666666
             }
-
-            self.predictions.append(prediction)
-            self.groundtruths.append(groundtruth)
-
-    def test_init(self):
-        """test metric init method."""
-        # test invalid normalized_items
-        with self.assertRaisesRegex(
-                KeyError, "Should be one of 'bbox', 'head', 'torso'"):
-            PCKAccuracy(norm_item='invalid')
-
-    def test_evaluate(self):
-        """test PCK accuracy evaluation metric."""
-        # test normalized by 'bbox'
-        pck_metric = PCKAccuracy(thr=0.5, norm_item='bbox')
-        pck_results = pck_metric(self.predictions, self.groundtruths)
-        target = {'PCK@0.5': 1.0}
-        self.assertDictEqual(pck_results, target)
-
-        # test normalized by 'head_size'
-        pckh_metric = PCKAccuracy(thr=0.3, norm_item='head')
-        pckh_results = pckh_metric(self.predictions, self.groundtruths)
-        target = {'PCKh@0.3': 1.0}
-        self.assertDictEqual(pckh_results, target)
-
-        # test normalized by 'torso_size'
-        tpck_metric = PCKAccuracy(thr=0.05, norm_item=['bbox', 'torso'])
-        tpck_results = tpck_metric(self.predictions, self.groundtruths)
-        self.assertIsInstance(tpck_results, dict)
-        target = {
-            'PCK@0.05': 1.0,
-            'tPCK@0.05': 1.0,
-        }
-        self.assertDictEqual(tpck_results, target)
+        )
+    ]
+)
+# yapf: enable
+def test_jhmdb_pck_accuracy_accurate(metric_kwargs, prediction, groundtruth,
+                                     results):
+    jhmdb_pck_accuracy = JhmdbPCKAccuracy(**metric_kwargs)
+    metric_results = jhmdb_pck_accuracy([prediction], [groundtruth])
+    for key in metric_results:
+        np.testing.assert_allclose(metric_results[key], results[key])
 
 
-class TestMpiiPCKAccuracy(TestCase):
+# yapf: disable
+@pytest.mark.parametrize(
+    argnames='metric_kwargs',
+    argvalues=[
+        {},
+        {'thr': 1.0, 'norm_item': 'bbox'},
+        {'thr': 0.2, 'norm_item': 'head'},
+        {'thr': 0.2, 'norm_item': 'torso'},
+        {'thr': 0.2, 'norm_item': ['bbox', 'torso']},
+    ]
+)
+# yapf: enable
+def test_mpii_pck_accuracy_interface(metric_kwargs):
+    num_keypoints = 16
+    keypoints = np.random.random((1, num_keypoints, 2)) * 10
+    prediction = {'coords': keypoints}
+    keypoints_visible = np.ones((1, num_keypoints)).astype(bool)
+    groundtruth = {
+        'coords': keypoints,
+        'mask': keypoints_visible,
+        'bbox_size': np.random.random((1, 2)) * 10,  # for `bbox` norm_item
+        'head_size': np.random.random((1, 2)) * 10,  # for `head` norm_item
+        'torso_size': np.random.random((1, 2)) * 10,  # for `torso` norm_item
+    }
+    mpii_pck_accuracy = MpiiPCKAccuracy(**metric_kwargs)
+    results = mpii_pck_accuracy([prediction], [groundtruth])
+    assert isinstance(results, dict)
 
-    def setUp(self):
-        """Setup some variables which are used in every test method.
 
-        TestCase calls functions in this order: setUp() -> testMethod() ->
-        tearDown() -> cleanUp()
-        """
-        self.batch_size = 8
-        num_keypoints = 16
-        self.predictions = []
-        self.groundtruths = []
-
-        for i in range(self.batch_size):
-            keypoints = np.zeros((1, num_keypoints, 2))
-            keypoints[0, i] = [0.5 * i, 0.5 * i]
-            keypoints_visible = np.ones((1, num_keypoints)).astype(bool)
-            keypoints_visible[0, (2 * i) % 8] = False
-            bbox_size = np.random.random((1, 2)) * 20 * i
-            head_size = np.random.random((1, 2)) * 10 * i
-            torso_size = np.random.random((1, 2)) * 15 * i
-
-            prediction = {'coords': keypoints}
-            groundtruth = {
-                'coords': keypoints + 1.0,
-                'mask': keypoints_visible,
-                'bbox_size': bbox_size,
-                'head_size': head_size,
-                'torso_size': torso_size
+# yapf: disable
+@pytest.mark.parametrize(
+    argnames=['metric_kwargs', 'prediction', 'groundtruth', 'results'],
+    argvalues=[
+        (
+            {'thr': 0.2, 'norm_item': 'head'},
+            {
+                'coords': np.array([[
+                    [2, 4], [6, 8], [6, 9], [7, 8], [3, 4],
+                    [7, 8], [8, 9], [8, 8], [1, 9], [2, 8],
+                    [4, 8], [6, 9], [6, 2], [1, 2], [2, 3],
+                    [9, 9]]])
+            },
+            {
+                'coords': np.array([[
+                    [1, 4], [6, 8], [6, 9], [7, 8], [3, 4],
+                    [1, 8], [8, 9], [8, 8], [1, 9], [2, 8],
+                    [1, 7], [6, 9], [6, 2], [1, 2], [2, 3],
+                    [9, 9]]]),
+                'mask': np.array([[
+                    True, True, True, True, True, True, True, True, True, True,
+                    True, True, True, True, True, True]]),
+                'head_size': np.array([[10, 10]])
+            },
+            {
+                'Head': 100,
+                'Shoulder': 100,
+                'Elbow': 100,
+                'Wrist': 50,
+                'Hip': 100,
+                'Knee': 100,
+                'Ankle': 0.0,
+                'PCKh': 78.57142857142856,
+                'PCKh@0.1': 0.0
             }
-
-            self.predictions.append(prediction)
-            self.groundtruths.append(groundtruth)
-
-    def test_init(self):
-        """test metric init method."""
-        # test invalid normalized_items
-        with self.assertRaisesRegex(
-                KeyError, "Should be one of 'bbox', 'head', 'torso'"):
-            MpiiPCKAccuracy(norm_item='invalid')
-
-    def test_evaluate(self):
-        """test PCK accuracy evaluation metric."""
-        # test normalized by 'head_size'
-        mpii_pckh_metric = MpiiPCKAccuracy(thr=0.3, norm_item='head')
-        pckh_results = mpii_pckh_metric(self.predictions, self.groundtruths)
-        target = {
-            'Head': 100.0,
-            'Shoulder': 100.0,
-            'Elbow': 100.0,
-            'Wrist': 100.0,
-            'Hip': 100.0,
-            'Knee': 100.0,
-            'Ankle': 100.0,
-            'PCKh': 100.0,
-            'PCKh@0.1': 100.0,
-        }
-        self.assertDictEqual(pckh_results, target)
+        )
+    ]
+)
+# yapf: enable
+def test_mpii_pck_accuracy_accurate(metric_kwargs, prediction, groundtruth,
+                                    results):
+    mpii_pck_accuracy = MpiiPCKAccuracy(**metric_kwargs)
+    metric_results = mpii_pck_accuracy([prediction], [groundtruth])
+    for key in metric_results:
+        np.testing.assert_allclose(metric_results[key], results[key])
 
 
-class TestJhmdbPCKAccuracy(TestCase):
-
-    def setUp(self):
-        """Setup some variables which are used in every test method.
-
-        TestCase calls functions in this order: setUp() -> testMethod() ->
-        tearDown() -> cleanUp()
-        """
-        self.batch_size = 8
-        num_keypoints = 15
-        self.predictions = []
-        self.groundtruths = []
-
-        for i in range(self.batch_size):
-            keypoints = np.zeros((1, num_keypoints, 2))
-            keypoints[0, i] = [0.5 * i, 0.5 * i]
-            keypoints_visible = np.ones((1, num_keypoints)).astype(bool)
-            keypoints_visible[0, (2 * i) % 8] = False
-            bbox_size = np.random.random((1, 2)) * 20 * i
-            head_size = np.random.random((1, 2)) * 10 * i
-            torso_size = np.random.random((1, 2)) * 15 * i
-
-            prediction = {'coords': keypoints}
-            groundtruth = {
-                'coords': keypoints,
-                'mask': keypoints_visible,
-                'bbox_size': bbox_size,
-                'head_size': head_size,
-                'torso_size': torso_size
-            }
-
-            self.predictions.append(prediction)
-            self.groundtruths.append(groundtruth)
-
-    def test_init(self):
-        """test metric init method."""
-        # test invalid normalized_items
-        with self.assertRaisesRegex(
-                KeyError, "Should be one of 'bbox', 'head', 'torso'"):
-            JhmdbPCKAccuracy(norm_item='invalid')
-
-    def test_evaluate(self):
-        """test PCK accuracy evaluation metric."""
-        # test normalized by 'bbox_size'
-        jhmdb_pck_metric = JhmdbPCKAccuracy(thr=0.5, norm_item='bbox')
-        pckh_results = jhmdb_pck_metric(self.predictions, self.groundtruths)
-        target = {
-            'PCK@0.5': 1.0,
-            'Head PCK': 1.0,
-            'Sho PCK': 1.0,
-            'Elb PCK': 1.0,
-            'Wri PCK': 1.0,
-            'Hip PCK': 1.0,
-            'Knee PCK': 1.0,
-            'Ank PCK': 1.0,
-            'Mean PCK': 1.0,
-        }
-        self.assertDictEqual(pckh_results, target)
-
-        # test normalized by 'torso_size'
-        jhmdb_tpck_metric = JhmdbPCKAccuracy(thr=0.2, norm_item='torso')
-        tpck_results = jhmdb_tpck_metric(self.predictions, self.groundtruths)
-        target = {
-            'Head tPCK': 1.0,
-            'Sho tPCK': 1.0,
-            'Elb tPCK': 1.0,
-            'Wri tPCK': 1.0,
-            'Hip tPCK': 1.0,
-            'Knee tPCK': 1.0,
-            'Ank tPCK': 1.0,
-            'Mean tPCK': 1.0,
-        }
-        self.assertDictEqual(tpck_results, target)
+if __name__ == '__main__':
+    pytest.main([__file__, '-v', '--capture=no'])
