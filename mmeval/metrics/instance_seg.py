50a257dbd8ac296dc2c644ca50e540a9d2a32a94
Pzzzzz <hello-cd.plus@hotmail.com>
2023-01-05 18:21:59 +0800

    update according to code review

--- mmeval/metrics/instance_seg.py
+++ mmeval/metrics/instance_seg.py
@@ -1,123 +1,22 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import numpy as np
+import warnings
 from copy import deepcopy
-from typing import Dict, List, Sequence
+from typing import Dict, List, Optional, Sequence
 
 from mmeval.core.base_metric import BaseMetric
 from mmeval.metrics._vendor.scannet import scannet_eval
-
-
-def aggregate_predictions(masks, labels, scores, valid_class_ids):
-    """Maps predictions to ScanNet evaluator format.
-
-    Args:
-        masks (list[numpy.ndarray]): Per scene predicted instance masks.
-        labels (list[numpy.ndarray]): Per scene predicted instance labels.
-        scores (list[numpy.ndarray]): Per scene predicted instance scores.
-        valid_class_ids (tuple[int]): Ids of valid categories.
-
-    Returns:
-        list[dict]: Per scene aggregated predictions.
-    """
-    infos = []
-    for id, (mask, label, score) in enumerate(zip(masks, labels, scores)):
-        info = dict()
-        n_instances = mask.max() + 1
-        for i in range(n_instances):
-            # match pred_instance['filename'] from assign_instances_for_scan
-            file_name = f'{id}_{i}'
-            info[file_name] = dict()
-            info[file_name]['mask'] = (mask == i).astype(int)
-            info[file_name]['label_id'] = valid_class_ids[label[i]]
-            info[file_name]['conf'] = score[i]
-        infos.append(info)
-    return infos
-
-
-def rename_gt(gt_semantic_masks, gt_instance_masks, valid_class_ids):
-    """Maps gt instance and semantic masks to instance masks for ScanNet
-    evaluator.
-
-    Args:
-        gt_semantic_masks (list[numpy.ndarray]): Per scene gt semantic masks.
-        gt_instance_masks (list[numpy.ndarray]): Per scene gt instance masks.
-        valid_class_ids (tuple[int]): Ids of valid categories.
-
-    Returns:
-        list[np.array]: Per scene instance masks.
-    """
-    renamed_instance_masks = []
-    for semantic_mask, instance_mask in zip(gt_semantic_masks,
-                                            gt_instance_masks):
-        unique = np.unique(instance_mask)
-        assert len(unique) < 1000
-        for i in unique:
-            semantic_instance = semantic_mask[instance_mask == i]
-            semantic_unique = np.unique(semantic_instance)
-            assert len(semantic_unique) == 1
-            if semantic_unique[0] < len(valid_class_ids):
-                instance_mask[
-                    instance_mask ==
-                    i] = 1000 * valid_class_ids[semantic_unique[0]] + i
-        renamed_instance_masks.append(instance_mask)
-    return renamed_instance_masks
-
-
-def instance_seg_eval(gt_semantic_masks,
-                      gt_instance_masks,
-                      pred_instance_masks,
-                      pred_instance_labels,
-                      pred_instance_scores,
-                      valid_class_ids,
-                      class_labels,
-                      options=None):
-    """Instance Segmentation Evaluation.
-
-    Evaluate the result of the instance segmentation.
-
-    Args:
-        gt_semantic_masks (list[numpy.ndarray]): Ground truth semantic masks.
-        gt_instance_masks (list[numpy.ndarray]): Ground truth instance masks.
-        pred_instance_masks (list[numpy.ndarray]): Predicted instance masks.
-        pred_instance_labels (list[numpy.ndarray]): Predicted instance labels.
-        pred_instance_scores (list[numpy.ndarray]): Predicted instance scores.
-        valid_class_ids (tuple[int]): Ids of valid categories.
-        class_labels (tuple[str]): Names of valid categories.
-        options (dict, optional): Additional options. Keys may contain:
-            `overlaps`, `min_region_sizes`, `distance_threshes`,
-            `distance_confs`. Default: None.
-        logger (logging.Logger | str, optional): The way to print the mAP
-            summary. See `mmdet.utils.print_log()` for details. Default: None.
-
-    Returns:
-        dict[str, float]: Dict of results.
-    """
-    assert len(valid_class_ids) == len(class_labels)
-    id_to_label = {
-        valid_class_ids[i]: class_labels[i]
-        for i in range(len(valid_class_ids))
-    }
-    preds = aggregate_predictions(
-        masks=pred_instance_masks,
-        labels=pred_instance_labels,
-        scores=pred_instance_scores,
-        valid_class_ids=valid_class_ids)
-    gts = rename_gt(gt_semantic_masks, gt_instance_masks, valid_class_ids)
-    metrics = scannet_eval(
-        preds=preds,
-        gts=gts,
-        options=options,
-        valid_class_ids=valid_class_ids,
-        class_labels=class_labels,
-        id_to_label=id_to_label)
-    return metrics
 
 
 class InstanceSeg(BaseMetric):
     """3D instance segmentation evaluation metric.
 
     Args:
-        dataset_meta (dict): Provide dataset meta information.
+        dataset_meta (dict, optional): Provide dataset meta information.
+        classes (List[str], optional): Provide dataset classes information as
+            an alternative to dataset_meta.
+        valid_class_ids (List[int], optional): Provide dataset valid class ids
+            information as an alternative to dataset_meta.
 
     Example:
         >>> import numpy as np
@@ -203,11 +102,26 @@ class InstanceSeg(BaseMetric):
     }
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self,
+                 classes: Optional[List[str]] = None,
+                 valid_class_ids: Optional[List[int]] = None,
+                 **kwargs):
         super().__init__(**kwargs)
-        assert self.dataset_meta is not None
-        self.classes = self.dataset_meta['classes']
-        self.valid_class_ids = self.dataset_meta['seg_valid_class_ids']
+        assert (self.dataset_meta
+                is not None) or (classes is not None
+                                 and valid_class_ids is not None)
+        if self.dataset_meta is not None:
+            if classes is not None or valid_class_ids is not None:
+                warnings.warn('`classes` and `valid_class_ids` are ignored.')
+            self.classes = self.dataset_meta['classes']
+            self.valid_class_ids = self.dataset_meta['seg_valid_class_ids']
+        else:
+            if not isinstance(classes, list):
+                raise TypeError('`classes` should be List[str]')
+            if not isinstance(valid_class_ids, list):
+                raise TypeError('`valid_class_ids` should be List[int]')
+            self.classes = classes
+            self.valid_class_ids = valid_class_ids
 
     def add(self, predictions: Sequence[Dict], groundtruths: Sequence[Dict]) -> None:  # type: ignore # yapf: disable # noqa: E501
         """Process one batch of data samples and predictions.
@@ -215,21 +129,21 @@ class InstanceSeg(BaseMetric):
         The processed results should be stored in ``self.results``,
         which will be used to compute the metrics when all batches
         have been processed.
-        
+
         Args:
             predictions (Sequence[Dict]): A sequence of dict. Each dict
                 representing a detection result, with the following keys:
 
-                - pts_instance_mask(numpy.ndarray): Predicted instance masks.
-                - instance_labels(numpy.ndarray): Predicted instance labels.
-                - instance_scores(numpy.ndarray): Predicted instance scores.
-            
+                - pts_instance_mask(np.ndarray): Predicted instance masks.
+                - instance_labels(np.ndarray): Predicted instance labels.
+                - instance_scores(np.ndarray): Predicted instance scores.
+
             groundtruths (Sequence[Dict]): A sequence of dict. Each dict
                 represents a groundtruths for an image, with the following
                 keys:
-            
-                - pts_instance_mask(numpy.ndarray): Ground truth instance masks.
-                - pts_semantic_mask(numpy.ndarray): Ground truth semantic masks.
+
+                - pts_instance_mask(np.ndarray): Ground truth instance masks.
+                - pts_semantic_mask(np.ndarray): Ground truth semantic masks.
         """
         for prediction, groundtruth in zip(predictions, groundtruths):
             self._results.append((deepcopy(prediction), deepcopy(groundtruth)))
@@ -239,7 +153,7 @@ class InstanceSeg(BaseMetric):
 
         Args:
             results (list): The processed results of each batch.
- 
+
         Returns:
             Dict[str, float]: The computed metrics. The keys are the names of
             the metrics, and the values are corresponding results.
@@ -257,7 +171,7 @@ class InstanceSeg(BaseMetric):
             pred_instance_labels.append(result_pred['instance_labels'])
             pred_instance_scores.append(result_pred['instance_scores'])
 
-        ret_dict = instance_seg_eval(
+        ret_dict = self.instance_seg_eval(
             gt_semantic_masks,
             gt_instance_masks,
             pred_instance_masks,
@@ -267,3 +181,108 @@ class InstanceSeg(BaseMetric):
             class_labels=self.classes)
 
         return ret_dict
+
+    def aggregate_predictions(self, masks, labels, scores, valid_class_ids):
+        """Maps predictions to ScanNet evaluator format.
+
+        Args:
+            masks (list[np.ndarray]): Per scene predicted instance masks.
+            labels (list[np.ndarray]): Per scene predicted instance labels.
+            scores (list[np.ndarray]): Per scene predicted instance scores.
+            valid_class_ids (tuple[int]): Ids of valid categories.
+
+        Returns:
+            list[dict]: Per scene aggregated predictions.
+        """
+        infos = []
+        for id, (mask, label, score) in enumerate(zip(masks, labels, scores)):
+            info = dict()
+            n_instances = mask.max() + 1
+            for i in range(n_instances):
+                file_name = f'{id}_{i}'
+                info[file_name] = dict()
+                info[file_name]['mask'] = (mask == i).astype(int)
+                info[file_name]['label_id'] = valid_class_ids[label[i]]
+                info[file_name]['conf'] = score[i]
+            infos.append(info)
+        return infos
+
+    def rename_gt(self, gt_semantic_masks, gt_instance_masks, valid_class_ids):
+        """Maps gt instance and semantic masks to instance masks for ScanNet
+        evaluator.
+
+        Args:
+            gt_semantic_masks (list[np.ndarray]): Per scene gt semantic masks.
+            gt_instance_masks (list[np.ndarray]): Per scene gt instance masks.
+            valid_class_ids (tuple[int]): Ids of valid categories.
+
+        Returns:
+            list[np.array]: Per scene instance masks.
+        """
+        renamed_instance_masks = []
+        for semantic_mask, instance_mask in zip(gt_semantic_masks,
+                                                gt_instance_masks):
+            unique = np.unique(instance_mask)
+            assert len(unique) < 1000
+            for i in unique:
+                semantic_instance = semantic_mask[instance_mask == i]
+                semantic_unique = np.unique(semantic_instance)
+                assert len(semantic_unique) == 1
+                if semantic_unique[0] < len(valid_class_ids):
+                    instance_mask[
+                        instance_mask ==
+                        i] = 1000 * valid_class_ids[semantic_unique[0]] + i
+            renamed_instance_masks.append(instance_mask)
+        return renamed_instance_masks
+
+    def instance_seg_eval(self,
+                          gt_semantic_masks,
+                          gt_instance_masks,
+                          pred_instance_masks,
+                          pred_instance_labels,
+                          pred_instance_scores,
+                          valid_class_ids,
+                          class_labels,
+                          options=None):
+        """Instance Segmentation Evaluation.
+
+        Evaluate the result of the instance segmentation.
+
+        Args:
+            gt_semantic_masks (list[np.ndarray]): Ground truth semantic masks.
+            gt_instance_masks (list[np.ndarray]): Ground truth instance masks.
+            pred_instance_masks (list[np.ndarray]): Predicted instance masks.
+            pred_instance_labels (list[np.ndarray]): Predicted instance labels.
+            pred_instance_scores (list[np.ndarray]): Predicted instance scores.
+            valid_class_ids (tuple[int]): Ids of valid categories.
+            class_labels (tuple[str]): Names of valid categories.
+            options (dict, optional): Additional options. Keys may contain:
+                `overlaps`, `min_region_sizes`, `distance_threshes`,
+                `distance_confs`. Default: None.
+            logger (logging.Logger | str, optional): The way to print the
+                mAP summary. See `mmdet.utils.print_log()` for details.
+                Default: None.
+
+        Returns:
+            dict[str, float]: Dict of results.
+        """
+        assert len(valid_class_ids) == len(class_labels)
+        id_to_label = {
+            valid_class_ids[i]: class_labels[i]
+            for i in range(len(valid_class_ids))
+        }
+        preds = self.aggregate_predictions(
+            masks=pred_instance_masks,
+            labels=pred_instance_labels,
+            scores=pred_instance_scores,
+            valid_class_ids=valid_class_ids)
+        gts = self.rename_gt(gt_semantic_masks, gt_instance_masks,
+                             valid_class_ids)
+        metrics = scannet_eval(
+            preds=preds,
+            gts=gts,
+            options=options,
+            valid_class_ids=valid_class_ids,
+            class_labels=class_labels,
+            id_to_label=id_to_label)
+        return metrics
--- tests/test_metrics/test_instance_seg.py
+++ tests/test_metrics/test_instance_seg.py
@@ -1,91 +1,91 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import numpy as np
+import pytest
 import torch
-import unittest
 
 from mmeval.metrics import InstanceSeg
 
 
-class TestInstanceSegMetric(unittest.TestCase):
+def _demo_mm_model_output():
+    """Create a superset of inputs needed to run test or train batches."""
+    n_points_list = [3300, 3000]
+    gt_labels_list = [[0, 0, 0, 0, 0, 0, 14, 14, 2, 1],
+                      [13, 13, 2, 1, 3, 3, 0, 0, 0]]
+    predictions = []
+    groundtruths = []
+    for n_points, gt_labels in zip(n_points_list, gt_labels_list):
+        gt_instance_mask = np.ones(n_points, dtype=int) * -1
+        gt_semantic_mask = np.ones(n_points, dtype=int) * -1
+        for i, gt_label in enumerate(gt_labels):
+            begin = i * 300
+            end = begin + 300
+            gt_instance_mask[begin:end] = i
+            gt_semantic_mask[begin:end] = gt_label
+        ann_info_data = dict()
+        ann_info_data['pts_instance_mask'] = torch.tensor(gt_instance_mask)
+        ann_info_data['pts_semantic_mask'] = torch.tensor(gt_semantic_mask)
+        results_dict = dict()
+        pred_instance_mask = np.ones(n_points, dtype=int) * -1
+        labels = []
+        scores = []
+        for i, gt_label in enumerate(gt_labels):
+            begin = i * 300
+            end = begin + 300
+            pred_instance_mask[begin:end] = i
+            labels.append(gt_label)
+            scores.append(.99)
+        results_dict['pts_instance_mask'] = torch.tensor(pred_instance_mask)
+        results_dict['instance_labels'] = torch.tensor(labels)
+        results_dict['instance_scores'] = torch.tensor(scores)
+        predictions.append(results_dict)
+        groundtruths.append(ann_info_data)
+    return predictions, groundtruths
 
-    def _demo_mm_model_output(self):
-        """Create a superset of inputs needed to run test or train batches."""
 
-        n_points_list = [3300, 3000]
-        gt_labels_list = [[0, 0, 0, 0, 0, 0, 14, 14, 2, 1],
-                          [13, 13, 2, 1, 3, 3, 0, 0, 0]]
+def test_metric_invalid_usage():
+    with pytest.raises(AssertionError):
+        InstanceSeg()
 
-        predictions = []
-        groundtruths = []
+    with pytest.raises(AssertionError):
+        InstanceSeg(
+            valid_class_ids=(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 24, 28,
+                             33, 34, 36, 39))
 
-        for n_points, gt_labels in zip(n_points_list, gt_labels_list):
-            gt_instance_mask = np.ones(n_points, dtype=int) * -1
-            gt_semantic_mask = np.ones(n_points, dtype=int) * -1
-            for i, gt_label in enumerate(gt_labels):
-                begin = i * 300
-                end = begin + 300
-                gt_instance_mask[begin:end] = i
-                gt_semantic_mask[begin:end] = gt_label
+    with pytest.raises(TypeError):
+        InstanceSeg(classes='a', valid_class_ids='b')
 
-            ann_info_data = dict()
-            ann_info_data['pts_instance_mask'] = torch.tensor(gt_instance_mask)
-            ann_info_data['pts_semantic_mask'] = torch.tensor(gt_semantic_mask)
 
-            results_dict = dict()
-            pred_instance_mask = np.ones(n_points, dtype=int) * -1
-            labels = []
-            scores = []
-            for i, gt_label in enumerate(gt_labels):
-                begin = i * 300
-                end = begin + 300
-                pred_instance_mask[begin:end] = i
-                labels.append(gt_label)
-                scores.append(.99)
-
-            results_dict['pts_instance_mask'] = torch.tensor(
-                pred_instance_mask)
-            results_dict['instance_labels'] = torch.tensor(labels)
-            results_dict['instance_scores'] = torch.tensor(scores)
-
-            predictions.append(results_dict)
-            groundtruths.append(ann_info_data)
-
-        return predictions, groundtruths
-
-    def test_evaluate(self):
-        predictions, groundtruths = self._demo_mm_model_output()
-        predictions = [{k: v.clone().numpy()
-                        for k, v in i.items()} for i in predictions]
-        groundtruths = [{k: v.clone().numpy()
-                         for k, v in i.items()} for i in groundtruths]
-        seg_valid_class_ids = (3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 24, 28,
-                               33, 34, 36, 39)
-        class_labels = ('cabinet', 'bed', 'chair', 'sofa', 'table', 'door',
-                        'window', 'bookshelf', 'picture', 'counter', 'desk',
-                        'curtain', 'refrigerator', 'showercurtrain', 'toilet',
-                        'sink', 'bathtub', 'garbagebin')
-        dataset_meta = dict(
-            seg_valid_class_ids=seg_valid_class_ids, classes=class_labels)
-        instance_seg_metric = InstanceSeg(dataset_meta=dataset_meta)
-        res = instance_seg_metric(predictions, groundtruths)
-        self.assertIsInstance(res, dict)
-        for label in [
-                'cabinet', 'bed', 'chair', 'sofa', 'showercurtrain', 'toilet'
-        ]:
-            metrics = res['classes'][label]
-            assert metrics['ap'] == 1.0
-            assert metrics['ap50%'] == 1.0
-            assert metrics['ap25%'] == 1.0
-
-        predictions[1]['pts_instance_mask'][2240:2700] = -1
-        predictions[0]['pts_instance_mask'][2700:3000] = 8
-        predictions[0]['instance_labels'][9] = 2
-
-        res = instance_seg_metric(predictions, groundtruths)
-
-        assert abs(res['classes']['cabinet']['ap50%'] - 0.72916) < 0.01
-        assert abs(res['classes']['cabinet']['ap25%'] - 0.88888) < 0.01
-        assert abs(res['classes']['bed']['ap50%'] - 0.5) < 0.01
-        assert abs(res['classes']['bed']['ap25%'] - 0.5) < 0.01
-        assert abs(res['classes']['chair']['ap50%'] - 0.375) < 0.01
-        assert abs(res['classes']['chair']['ap25%'] - 1.0) < 0.01
+def test_evaluate():
+    predictions, groundtruths = _demo_mm_model_output()
+    predictions = [{k: v.clone().numpy()
+                    for k, v in i.items()} for i in predictions]
+    groundtruths = [{k: v.clone().numpy()
+                     for k, v in i.items()} for i in groundtruths]
+    seg_valid_class_ids = (3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 24, 28, 33,
+                           34, 36, 39)
+    class_labels = ('cabinet', 'bed', 'chair', 'sofa', 'table', 'door',
+                    'window', 'bookshelf', 'picture', 'counter', 'desk',
+                    'curtain', 'refrigerator', 'showercurtrain', 'toilet',
+                    'sink', 'bathtub', 'garbagebin')
+    dataset_meta = dict(
+        seg_valid_class_ids=seg_valid_class_ids, classes=class_labels)
+    instance_seg_metric = InstanceSeg(dataset_meta=dataset_meta)
+    res = instance_seg_metric(predictions, groundtruths)
+    assert isinstance(res, dict)
+    for label in [
+            'cabinet', 'bed', 'chair', 'sofa', 'showercurtrain', 'toilet'
+    ]:
+        metrics = res['classes'][label]
+        assert metrics['ap'] == 1.0
+        assert metrics['ap50%'] == 1.0
+        assert metrics['ap25%'] == 1.0
+    predictions[1]['pts_instance_mask'][2240:2700] = -1
+    predictions[0]['pts_instance_mask'][2700:3000] = 8
+    predictions[0]['instance_labels'][9] = 2
+    res = instance_seg_metric(predictions, groundtruths)
+    assert abs(res['classes']['cabinet']['ap50%'] - 0.72916) < 0.01
+    assert abs(res['classes']['cabinet']['ap25%'] - 0.88888) < 0.01
+    assert abs(res['classes']['bed']['ap50%'] - 0.5) < 0.01
+    assert abs(res['classes']['bed']['ap25%'] - 0.5) < 0.01
+    assert abs(res['classes']['chair']['ap50%'] - 0.375) < 0.01
+    assert abs(res['classes']['chair']['ap25%'] - 1.0) < 0.01
