8c6e46ef44727e5c705115699b3961e32fcc899f
xuan07472 <xuan07472@gmail.com>
2023-01-13 16:32:44 +0800

    update test

--- tests/test_metrics/test_gradient_error.py
+++ tests/test_metrics/test_gradient_error.py
@@ -12,8 +12,7 @@ def test_gradient_error():
     trimap[:16, :16] = 128
     trimap[16:, 16:] = 255
 
-    gradienterror = GradientError()
-    gradienterror_results = gradienterror(pred_alpha, gt_alpha, trimap)
-    assert isinstance(gradienterror_results, dict)
-    np.testing.assert_almost_equal(gradienterror_results['GradientError'],
-                                   0.0935)
+    gradient_error = GradientError()
+    metric_results = gradient_error(pred_alpha, gt_alpha, trimap)
+    assert isinstance(metric_results, dict)
+    np.testing.assert_almost_equal(metric_results['GradientError'], 0.0935)
