f6465fa60feb0ec7e448cb0d39d2b9f0e78719c2
xuan07472 <xuan07472@gmail.com>
2023-01-13 18:28:15 +0800

    add connectivity error

--- mmeval/metrics/__init__.py
+++ mmeval/metrics/__init__.py
@@ -4,6 +4,7 @@ from .accuracy import Accuracy
 from .ava_map import AVAMeanAP
 from .bleu import BLEU
 from .coco_detection import COCODetectionMetric
+from .connectivity_error import ConnectivityError
 from .end_point_error import EndPointError
 from .f_metric import F1Metric
 from .hmean_iou import HmeanIoU
@@ -25,5 +26,5 @@ __all__ = [
     'F1Metric', 'HmeanIoU', 'SingleLabelMetric', 'COCODetectionMetric',
     'PCKAccuracy', 'MpiiPCKAccuracy', 'JhmdbPCKAccuracy', 'ProposalRecall',
     'PSNR', 'MAE', 'MSE', 'SSIM', 'SNR', 'MultiLabelMetric',
-    'AveragePrecision', 'AVAMeanAP', 'BLEU'
+    'AveragePrecision', 'AVAMeanAP', 'BLEU', 'ConnectivityError'
 ]
