ad879068773359c91b29e1a23312123c6a1e01b3
LareinaM <mhsj16lareina@gmail.com>
2023-02-15 11:15:57 +0800

    Add testcase use ``add`` twice then ``compute``

--- tests/test_metrics/test_keypoint_auc.py
+++ tests/test_metrics/test_keypoint_auc.py
@@ -13,35 +13,55 @@ class TestKeypointAUCandEPE(TestCase):
         TestCase calls functions in this order: setUp() -> testMethod() ->
         tearDown() -> cleanUp()
         """
-        output = np.zeros((1, 5, 2))
-        target = np.zeros((1, 5, 2))
+        self.output = np.zeros((1, 5, 2))
+        self.target = np.zeros((1, 5, 2))
         # first channel
-        output[0, 0] = [10, 4]
-        target[0, 0] = [10, 0]
+        self.output[0, 0] = [10, 4]
+        self.target[0, 0] = [10, 0]
         # second channel
-        output[0, 1] = [10, 18]
-        target[0, 1] = [10, 10]
+        self.output[0, 1] = [10, 18]
+        self.target[0, 1] = [10, 10]
         # third channel
-        output[0, 2] = [0, 0]
-        target[0, 2] = [0, -1]
+        self.output[0, 2] = [0, 0]
+        self.target[0, 2] = [0, -1]
         # fourth channel
-        output[0, 3] = [40, 40]
-        target[0, 3] = [30, 30]
+        self.output[0, 3] = [40, 40]
+        self.target[0, 3] = [30, 30]
         # fifth channel
-        output[0, 4] = [20, 10]
-        target[0, 4] = [0, 10]
+        self.output[0, 4] = [20, 10]
+        self.target[0, 4] = [0, 10]
 
-        keypoints_visible = np.array([[True, True, False, True, True]])
-
-        prediction = {'coords': output}
-        groundtruth = {'coords': target, 'mask': keypoints_visible}
-
-        self.predictions = [prediction]
-        self.groundtruths = [groundtruth]
+        self.keypoints_visible = np.array([[True, True, False, True, True]])
 
     def test_auc_evaluate(self):
         """test AUC evaluation metric."""
+        # case 1: norm_factor=20, num_thrs=4
         auc_metric = KeypointAUC(norm_factor=20, num_thrs=4)
-        auc_results = auc_metric(self.predictions, self.groundtruths)
         target = {'AUC@4': 0.375}
+
+        prediction = {'coords': self.output}
+        groundtruth = {'coords': self.target, 'mask': self.keypoints_visible}
+        predictions = [prediction]
+        groundtruths = [groundtruth]
+
+        auc_results = auc_metric(predictions, groundtruths)
+        self.assertDictEqual(auc_results, target)
+
+        # case 2: use ``add`` multiple times then ``compute``
+        auc_metric._results = []
+        preds1 = [{'coords': self.output[:3]}]
+        preds2 = [{'coords': self.output[3:]}]
+        gts1 = [{
+            'coords': self.target[:3],
+            'mask': self.keypoints_visible[:3]
+        }]
+        gts2 = [{
+            'coords': self.target[3:],
+            'mask': self.keypoints_visible[3:]
+        }]
+
+        auc_metric.add(preds1, gts1)
+        auc_metric.add(preds2, gts2)
+
+        auc_results = auc_metric.compute_metric(auc_metric._results)
         self.assertDictEqual(auc_results, target)
